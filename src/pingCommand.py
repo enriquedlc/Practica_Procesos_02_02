608ca1829c1297e7fef7d08dc1ffe99f56cbf95f
Enrique <enriquedlc2d@gmail.com>
2022-11-10 08:44:47 +0000

    Style(pingCommand.py): comments removed

--- src/pingCommand.py
+++ src/pingCommand.py
@@ -1,8 +1,6 @@
-# create a function to execute the ping command on host on windows and linux and join the output of the command to a string
-#
-
 import os
 import platform
+
 
 def pingCommand(host):
     if platform.system() == "Windows":
@@ -11,8 +9,7 @@ def pingCommand(host):
         output = os.popen("ping -c 2 " + host).read()
     return output
 
+
 if __name__ == "__main__":
     host = input("Enter the host to ping: ")
     print(pingCommand(host))
-
-
